4271cf3eb59c0f4efd00d1bdbf657aa83e561524
Ashley Rahl <asrahlcpt024@student.wethinkcode.co.za>
2026-05-21 18:05:20 +0200

    Getting user input's (num1, num2 and operator)

---
+++ calculator.py
@@ -0,0 +1,8 @@
+# Get user input for the first number
+num1 = float(input("Enter the first number: "))
+# Get user input for the second number
+num2 = float(input("Enter the second number: "))
+
+# Get user input for the operation
+operation = input("Enter the operation (+, -, *, /): ")
+
