d807f8d3fb7593ce2164b2cfe792d253c94cdf13
Ashley Rahl <asrahlcpt024@student.wethinkcode.co.za>
2026-05-21 18:10:49 +0200

    Testing if input works and if the correct value type

--- calculator.py
+++ calculator.py
@@ -6,3 +6,12 @@ num2 = float(input("Enter the second number: "))
 # Get user input for the operation
 operation = input("Enter the operation (+, -, *, /): ")
 
+# Testing the inputs
+print(num1)
+print(num2)
+print(operation)
+
+# Testing input type
+print(type(num1))
+print(type(num2))
+print(type(operation))
